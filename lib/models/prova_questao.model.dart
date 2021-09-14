fbe14c8a87fe87a5dee97acca4affaf002ddb262
Lucas Didur <ldidur@icloud.com>
2021-09-14 18:51:23 -0300

    feat: adicionado tipo de questao descritiva

--- lib/models/prova_questao.model.dart
+++ lib/models/prova_questao.model.dart
@@ -1,17 +1,21 @@
 import 'package:appserap/models/prova_alternativa.model.dart';
 
+enum EnumTipoQuestao { multiplaEscolha, descritiva }
+
 class ProvaQuestaoModel {
   int? id;
   String? titulo;
   String? descricao;
   int? ordem;
   List<ProvaAlternativaModel>? alternativas;
+  EnumTipoQuestao? tipo;
 
   ProvaQuestaoModel({
     required this.id,
     required this.titulo,
     required this.descricao,
     required this.ordem,
+    required this.tipo,
   });
 
   ProvaQuestaoModel.fromJson(Map<String, dynamic> json) {
@@ -19,9 +23,8 @@ class ProvaQuestaoModel {
     titulo = json['titulo'];
     descricao = json['descricao'];
     ordem = json['ordem'];
-    alternativas = json['alternativas'] != null
-        ? json['alternativas'].cast<ProvaAlternativaModel>()
-        : [];
+    alternativas = json['alternativas'] != null ? json['alternativas'].cast<ProvaAlternativaModel>() : [];
+    tipo = EnumTipoQuestao.values[json['tipo'] ?? 0];
   }
 
   Map<String, dynamic> toJson() {
@@ -30,6 +33,8 @@ class ProvaQuestaoModel {
     data['titulo'] = this.titulo;
     data['descricao'] = this.descricao;
     data['ordem'] = this.ordem;
+    data['tipo'] = this.tipo?.index ?? 0;
+
     return data;
   }
 
