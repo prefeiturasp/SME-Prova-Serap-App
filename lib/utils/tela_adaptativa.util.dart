12f467d8dd3565c7c3501d2411e907679894c71c
Erlon Jr <ejrgeek@gmail.com>
2021-11-03 08:39:32 -0300

    criado tela adaptativa

--- lib/utils/tela_adaptativa.util.dart
+++ lib/utils/tela_adaptativa.util.dart
@@ -1,23 +1,26 @@
 import 'dart:io';
 
+import 'package:appserap/enums/tipo_dispositivo.enum.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-enum TipoDispositivo { mobile, tablet, web }
-
 class TelaAdaptativaUtil {
-  TipoDispositivo dispositivo = TipoDispositivo.tablet;
+  EnumTipoDispositivo dispositivo = EnumTipoDispositivo.tablet;
 
   TelaAdaptativaUtil(){
     if (kIsWeb) {
-      dispositivo = TipoDispositivo.web;
+      dispositivo = EnumTipoDispositivo.web;
     } else if (Platform.isAndroid || Platform.isIOS) {
-      if ((ScreenUtil().orientation == Orientation.portrait && ScreenUtil.defaultSize.width < 600) ||
-          (ScreenUtil().orientation == Orientation.landscape && ScreenUtil.defaultSize.height < 600)) {
-        dispositivo = TipoDispositivo.mobile;
-      } else {
-        dispositivo = TipoDispositivo.tablet;
+      if (
+        (ScreenUtil().orientation == Orientation.portrait && ScreenUtil.defaultSize.width < 600)
+        ||
+        (ScreenUtil().orientation == Orientation.landscape && ScreenUtil.defaultSize.height < 600)
+        ) {
+        dispositivo = EnumTipoDispositivo.mobile;
+      } 
+      else {
+        dispositivo = EnumTipoDispositivo.tablet;
       }
     } 
   }
