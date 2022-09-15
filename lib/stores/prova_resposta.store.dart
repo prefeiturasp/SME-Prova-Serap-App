862fa938bd253b0c24cf0057350357dceeb0443e
Lucas Didur <ldidur@icloud.com>
2022-09-15 09:14:06 -0300

    fix(AB#73039 ): Remoção de respostas sincronizadas tambem ao finalizar a prova pelo botão de finalizar

--- lib/stores/prova_resposta.store.dart
+++ lib/stores/prova_resposta.store.dart
@@ -135,6 +135,8 @@ abstract class _ProvaRespostaStoreBase with Store, Loggable, Database {
       }
     }
 
+    await dbRespostas.respostaProvaDao.removerSincronizadas();
+
     fine('[$idProva] - Sincronização com o servidor servidor concluida');
   }
 
