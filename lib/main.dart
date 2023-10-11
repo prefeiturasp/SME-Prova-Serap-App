116c1c34fb44adb6ef9fdfb9bb3e5a71c02078b3
Lucas Didur <ldidur@icloud.com>
2023-10-11 18:31:32 -0300

    fix: alteracao de tempos de execucao

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:appserap/main.ioc.dart';
 import 'package:appserap/main.route.dart';
 import 'package:appserap/utils/app_config.util.dart';
 import 'package:appserap/utils/notificacao.util.dart';
+import 'package:appserap/utils/route_observer.dart';
 import 'package:appserap/utils/tela_adaptativa.util.dart';
 import 'package:appserap/utils/tema.util.dart';
 import 'package:appserap/workers/dispacher.dart';
@@ -163,7 +164,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp.router(
           routerConfig: _appRouter.config(
-            navigatorObservers: () => [RouteObserver()],
+            navigatorObservers: () => [RouterObserver()],
           ),
           debugShowCheckedModeBanner: false,
           theme: ThemeData.light().copyWith(
--- lib/utils/route_observer.dart
+++ lib/utils/route_observer.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-class RouteObserver extends AutoRouterObserver {
+class RouterObserver extends AutoRouterObserver {
   @override
   void didPush(Route route, Route? previousRoute) {
     debugPrint('New route pushed: ${route.settings.name}');
